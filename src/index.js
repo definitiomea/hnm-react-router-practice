ec9c8ea196734071b49601b16d8133e98ab6d477
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-15 20:20:19 +0900

    Revert "hnm 프로젝트 업로드"
    This reverts commit d6989488dde487fd3d9cb46f3c5571bb0a76ad87.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -27,6 +28,14 @@ root.render(
     <App />
   </BrowserRouter>
 >>>>>>> parent of 7decdb4 (practice, my-json-server 써보기)
+=======
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+>>>>>>> parent of d698948 (hnm 프로젝트 업로드)
 );
 
 // If you want to start measuring performance in your app, pass a function
