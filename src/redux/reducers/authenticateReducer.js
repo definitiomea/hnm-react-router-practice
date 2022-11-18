5e3f11f3d719039bc45a330d7f40e4fef79cceb0
Johnny_P <shinezl39@gmail.com>
2022-11-18 17:02:53 +0900

    주석 정리

--- src/redux/reducers/authenticateReducer.js
+++ src/redux/reducers/authenticateReducer.js
@@ -6,32 +6,6 @@ let initialState = {
     authenciate: false
 };
 
-/* function authenciateReducer(state = initialState, action) {
-    let { type, payload } = action;
-    switch(type) {
-        case "LOGIN_SUCCESS":
-            console.log("login reducer here");
-            return {
-                ...state,
-                id: payload.id,
-                password: payload.password,
-                authenciate: true
-            }
-        case "LOGOUT_SUCCESS":
-            console.log("logout reducer here");
-            return {
-                ...state,
-                id: "",
-                password: "",
-                authenciate: false
-            }
-        default:
-            return { ...state }
-    }
-}
-
-export default authenciateReducer; */
-
 const authenciateSlice = createSlice({
     name: "authenciate",
     initialState,
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,25 +5,6 @@ let initialState = {
     productDetailList: []
 };
 
-/* function productReducer(state = initialState, action) {
-    let { type, payload } = action;
-    switch(type) {
-        case "GET_PRODUCT_SUCCESS":
-            return {
-                ...state, productList: payload.data
-            };
-        case "GET_PRODUCT_DETAIL_SUCCESS":
-            console.log("detail reducer test");
-            return {
-                ...state, productDetailList: payload.data
-            }
-        default:
-            return { ...state };
-    }
-} */
-
-/* export default productReducer; */
-
 const productSlice = createSlice({
     name: "product",
     initialState,
