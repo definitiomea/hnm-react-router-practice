1b07d27e0d6fb3288a1e02261e3bd94b9acc3e86
Johnny_P <shinezl39@gmail.com>
2022-12-02 20:18:27 +0900

    주석 추가

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+/* Provider에는 store를 담아주기 */
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+/* 프로바이더는 여기, 리액트 리덕스에서 땡겨오기 */
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
